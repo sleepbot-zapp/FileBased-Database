d02bb8cde5341660133fc2875c5b68680db96ece
Neeladri Mullick <78042190+sleepbot-zapp@users.noreply.github.com>
2024-11-25 23:56:40 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="sqlike_database",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "cryptography>=39.0.0",
+    ],
+)
